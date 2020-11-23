f8643c232d0516126663782ba23024da46c577d1
yugo333 <yugo199103@gmail.com>
2020-11-23 14:51:32 +0900

    リファクタ

--- src/pages/sample/index.js
+++ src/pages/sample/index.js
@@ -1,7 +1,1 @@
-// import { testAlert } from '@js/sample.js';
 import "./style.scss";
-
-// const sampleButton = document.getElementById('sampleButton');
-// sampleButton.addEventListener('click', () => {
-//   testAlert(33);
-// });
