8bab290fb1eca7f30b280ba059827135667ff4b5
yugo333 <yugo199103@gmail.com>
2020-12-06 15:47:18 +0900

    fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,28 +4,38 @@ const commonConfig = require("./webpack.common");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
 const OptimizeCssPlugin = require("optimize-css-assets-webpack-plugin");
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const outputFile = "[name]";
 const assetFile = "[name]";
+// 静的ファイルを保存する場所によって変える。インフラ担当者と要相談。
 const assetPath = "/";
 
 module.exports = (env) => {
   // package.jsonのscriptで --env.envFile=で指定されたパスのenvFileを使用する。
-  // 指定されていない場合は.env.developmentを使用する
-  const envFilePath = env ? `./env/.env.${env.file}` : "./env/.env.development";
+  // 指定されていない場合は.env.productionを使用する
+  const envFilePath = env ? `./env/.env.${env.file}` : "./env/.env.production";
 
   // webpack.common.jsのentryで追加したhtmlファイルを動的に生成する。
   const createHtmlPlugins = (entry) => {
-    const htmpPlugins = [];
+    // 最初にdistディレクトリを空にする
+    const htmpPlugins = [new CleanWebpackPlugin()];
     Object.keys(entry).forEach((key) => {
       htmpPlugins.push(
         new HtmlWebpackPlugin({
           template: path.resolve(__dirname, `./src/pages/${key}.html`),
           // 出力されるファイル名
-          filename: `${key}.html`,
+          filename: `./pages/${key}.html`,
           // headにjsファイルを入れたい場合はheadを指定
           inject: "body",
+          minify: {
+            collapseWhitespace: true,
+            removeComments: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            useShortDoctype: true,
+          },
           // 読み込むjsファイルを指定
           chunks: [key],
         })
