64468e0606d205448f6e48a6a299f2da39418c43
yugo333 <yugo199103@gmail.com>
2020-12-13 03:31:09 +0900

    upDate

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,42 +2,29 @@ const path = require("path");
 const webpackMerge = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserWebpackPlugin = require("terser-webpack-plugin");
-const OptimizeCssPlugin = require("optimize-css-assets-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
-const outputFile = "[name].[chunkhash]";
-const assetFile = "[name].[contenthash]";
-// 静的ファイルを保存する場所によって変える。インフラ担当者と要相談。
+const outputFile = "[name]";
+const assetFile = "[name]";
 const assetPath = "/";
 
 module.exports = (env) => {
   // package.jsonのscriptで --env.envFile=で指定されたパスのenvFileを使用する。
-  // 指定されていない場合は.env.productionを使用する
-  const envFilePath = env ? `./env/.env.${env.file}` : "./env/.env.production";
+  // 指定されていない場合は.env.developmentを使用する
+  const envFilePath = env ? `./env/.env.${env.file}` : "./env/.env.development";
 
   // webpack.common.jsのentryで追加したhtmlファイルを動的に生成する。
   const createHtmlPlugins = (entry) => {
-    // 最初にdistディレクトリを空にする
+    const htmpPlugins = [];
     new UglifyJSPlugin();
-    const htmpPlugins = [new CleanWebpackPlugin()];
     Object.keys(entry).forEach((key) => {
       htmpPlugins.push(
         new HtmlWebpackPlugin({
           template: path.resolve(__dirname, `./src/pages/${key}.html`),
           // 出力されるファイル名
-          filename: `./pages/${key}.html`,
+          filename: `${key}.html`,
           // headにjsファイルを入れたい場合はheadを指定
           inject: "body",
-          minify: {
-            collapseWhitespace: true,
-            removeComments: true,
-            removeRedundantAttributes: true,
-            removeScriptTypeAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            useShortDoctype: true,
-          },
           // 読み込むjsファイルを指定
           chunks: [key],
         })
@@ -45,25 +32,27 @@ module.exports = (env) => {
     });
     return htmpPlugins;
   };
+
   return webpackMerge(
     commonConfig({ outputFile, assetFile, envFilePath, assetPath }),
     {
-      mode: "production",
+      mode: "development",
+      devtool: "inline-source-map",
       plugins: createHtmlPlugins(
         commonConfig({ outputFile, assetFile, envFilePath, assetPath }).entry
       ),
-      optimization: {
-        minimizer: [
-          // javascriptの最適化
-          new TerserWebpackPlugin({
-            terserOptions: {
-              // consoleを削除する
-              compress: { drop_console: true },
-            },
-          }),
-          // cssの最適化
-          new OptimizeCssPlugin(),
-        ],
+      devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        // どのブラウザを自動で立ち上げるか。trueで標準のブラウザ。デフォルトでは立ち上がらない。
+        // open: "Google Chrome",
+        host: "localhost",
+        compress: true,
+        // port番号はデフォルトで8080, 既に使用されている場合は自動で8181になる。指定したい場合はここでする。
+        // port: 3000,
+        watchOptions: {
+          // 差分を検知しないディレクトリ
+          ignored: /node_modules/,
+        },
       },
     }
   );
